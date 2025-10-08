3029db8384befcd732042f1a0c5f0f5b55bd3187
Julia-proj <juleea0@gmail.com>
2025-10-08 14:37:16 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -268,7 +268,7 @@ export default function App() {
 
         <style jsx>{`
           .hero-bg{
-            background-image: url('/images/IMG_6243.png');
+            background-image: url('/images/IMG_6243.jpeg');
             background-repeat: no-repeat;
             background-size: cover;
             background-position: right center;
